a01cc5daf7065ea2ae43a65b21c6c972a4b1f3e5
Hoa Ngo <hoa.ngo@journeyh.io>
2021-06-18 22:34:02 +0700

    Finished responsive web design

--- app.js
+++ app.js
@@ -13,9 +13,17 @@ window.onclick = (e) => {
 
 const myFunc = () => {
   const nav = document.getElementById('my-nav');
+  const icon = document.getElementById('my-icon');
+
   if (nav.className === 'main-nav') {
     nav.className += ' responsive';
+
+    icon.classList.remove('fa-bars');
+    icon.className += ' fa-times';
   } else {
     nav.className = 'main-nav';
+
+    icon.classList.remove('fa-times');
+    icon.className += ' fa-bars';
   }
 };
