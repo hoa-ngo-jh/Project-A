c3af1b474f21b9c7a3278eec4a915f68151fc620
Hoa Ngo <hoa.ngo@journeyh.io>
2021-06-29 09:30:22 +0700

    fixed layout, fonts,...

--- app.js
+++ app.js
@@ -1,12 +1,29 @@
-const toggleDropdown = () => {
-  document.getElementById('my-dropdown').classList.toggle('show');
+const toggleDropdown = (index) => {
+  let id = '';
+  if (index === 1) {
+    id = 'my-dropdown';
+  } else if (index === 2) {
+    id = 'my-dropdown-2';
+  } else {
+    id = 'my-dropdown-3';
+  }
+
+  document.getElementById(id).classList.toggle('show');
 };
 
 window.onclick = (e) => {
   if (!e.target.matches('.dropbtn')) {
     const myDropdown = document.getElementById('my-dropdown');
+    const myDropdown2 = document.getElementById('my-dropdown-2');
+    const myDropdown3 = document.getElementById('my-dropdown-3');
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
+    }
+    if (myDropdown2.classList.contains('show')) {
+      myDropdown2.classList.remove('show');
+    }
+    if (myDropdown3.classList.contains('show')) {
+      myDropdown3.classList.remove('show');
     }
   }
 };
